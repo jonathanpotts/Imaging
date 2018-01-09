361e6ba42d20c874bb7a62958044d9f96131e2a7
Jonathan Potts <jonathanpotts@outlook.com>
2018-01-08 19:23:21 -0800

    Commit code

---
+++ Imaging/Image.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+
+namespace JonathanPotts.Imaging
+{
+    /// <summary>
+    /// An image that supports basic manipulation functionality.
+    /// </summary>
+    public abstract class Image : IDisposable
+    {
+        /// <summary>
+        /// The width in pixels of the image being processed.
+        /// </summary>
+        public abstract int Width { get; }
+        /// <summary>
+        /// The height in pixels of the image being processed.
+        /// </summary>
+        public abstract int Height { get; }
+
+        /// <summary>
+        /// Creates a manipulatable image from a stream.
+        /// </summary>
+        /// <param name="stream">A stream containing the image data</param>
+        /// <returns>A manipulatable image</returns>
+        public static Image FromStream(Stream stream)
+        {
+            return ImageSkiaSharp.FromStream(stream);
+        }
+
+        /// <summary>
+        /// Provides a stream containing encoded image data.
+        /// </summary>
+        /// <param name="format">The format to encode the image</param>
+        /// <param name="quality">The quality (1-100) of the encoded the image</param>
+        /// <returns>A stream containing encoded image data</returns>
+        public abstract Stream AsEncodedStream(ImageEncodingFormat format, int quality = 100);
+        /// <summary>
+        /// Resizes the image to the specified width and height.
+        /// </summary>
+        /// <param name="width">The width in pixels to resize the image</param>
+        /// <param name="height">The height in pixels to resize the image</param>
+        public abstract void Resize(int width, int height);
+        /// <summary>
+        /// Crops the image using the specified aspect ratio.
+        /// </summary>
+        /// <param name="aspect">The aspect ratio to crop the image</param>
+        public abstract void Crop(double aspect);
+        /// <summary>
+        /// Removes the transparency from the image.
+        /// </summary>
+        /// <param name="color">The color to make the background of the image</param>
+        public abstract void RemoveTransparency(Color color);
+        /// <summary>
+        /// Disposes the image from memory.
+        /// </summary>
+        public abstract void Dispose();
+    }
+
+    /// <summary>
+    /// Formats that the image data can be encoded with.
+    /// </summary>
+    public enum ImageEncodingFormat
+    {
+        /// <summary>
+        /// Adapatable Scalable Texture Compression (ASTC)
+        /// </summary>
+        Astc,
+        /// <summary>
+        /// Bitmap (BMP)
+        /// </summary>
+        Bmp,
+        /// <summary>
+        /// Digital Negative (DNG)
+        /// </summary>
+        Dng,
+        /// <summary>
+        /// Graphics Interchange Format (GIF)
+        /// </summary>
+        Gif,
+        /// <summary>
+        /// Icon (ICO)
+        /// </summary>
+        Ico,
+        /// <summary>
+        /// Joint Photographic Experts Group (JPEG)
+        /// </summary>
+        Jpeg,
+        /// <summary>
+        /// Khronos Texture (KTX)
+        /// </summary>
+        Ktx,
+        /// <summary>
+        /// Ericsson Texture Compression - PKM (PKM)
+        /// </summary>
+        Pkm,
+        /// <summary>
+        /// Portable Network Graphics (PNG)
+        /// </summary>
+        Png,
+        /// <summary>
+        /// Wireless Application Protocol Bitmap (WBMP)
+        /// </summary>
+        Wbmp,
+        /// <summary>
+        /// WebP
+        /// </summary>
+        Webp
+    }
+
+    /// <summary>
+    /// Commonly used aspect ratios for cropping images.
+    /// </summary>
+    public static class ImageCropFactors
+    {
+        /// <summary>
+        /// Square 1:1
+        /// </summary>
+        public const double Square = 1.0;
+        /// <summary>
+        /// Standard 4:3
+        /// </summary>
+        public const double Standard4x3 = 4.0 / 3.0;
+        /// <summary>
+        /// Standard 3:2
+        /// </summary>
+        public const double Standard3x2 = 3.0 / 2.0;
+        /// <summary>
+        /// Widescreen 16:10
+        /// </summary>
+        public const double Widescreen16x10 = 16.0 / 10.0;
+        /// <summary>
+        /// Widescreen 16:9
+        /// </summary>
+        public const double Widescreen16x9 = 16.0 / 9.0;
+    }
+}
---
+++ Imaging/ImageSkiaSharp.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using SkiaSharp;
+
+namespace JonathanPotts.Imaging
+{
+    /// <summary>
+    /// An image that supports basic manipulation functionality using SkiaSharp.
+    /// </summary>
+    public class ImageSkiaSharp : Image
+    {
+        /// <summary>
+        /// The width in pixels of the image being processed.
+        /// </summary>
+        public override int Width
+        {
+            get
+            {
+                return _bitmap.Width;
+            }
+        }
+
+        /// <summary>
+        /// The height in pixels of the image being processed.
+        /// </summary>
+        public override int Height
+        {
+            get
+            {
+                return _bitmap.Height;
+            }
+        }
+
+        private SKBitmap _bitmap;
+
+        private ImageSkiaSharp()
+        {
+
+        }
+
+        /// <summary>
+        /// Creates a manipulatable image from a stream.
+        /// </summary>
+        /// <param name="stream">A stream containing the image data</param>
+        /// <returns>A manipulatable image</returns>
+        public static new ImageSkiaSharp FromStream(Stream stream)
+        {
+            using (var managedStream = new SKManagedStream(stream))
+            {
+                return new ImageSkiaSharp()
+                {
+                    _bitmap = SKBitmap.Decode(managedStream)
+                };
+            }
+        }
+
+        /// <summary>
+        /// Provides a stream containing encoded image data.
+        /// </summary>
+        /// <param name="format">The format to encode the image</param>
+        /// <param name="quality">The quality (1-100) of the encoded the image</param>
+        /// <returns>A stream containing encoded image data</returns>
+        public override Stream AsEncodedStream(ImageEncodingFormat format, int quality = 100)
+        {
+            if (quality < 1 || quality > 100)
+            {
+                throw new ArgumentOutOfRangeException("quality", "The quality specified needs to be between 1 and 100.");
+            }
+
+            using (var image = SKImage.FromBitmap(_bitmap))
+            {
+                SKEncodedImageFormat imageFormat;
+
+                if (format == ImageEncodingFormat.Astc)
+                {
+                    imageFormat = SKEncodedImageFormat.Astc;
+                }
+                else if (format == ImageEncodingFormat.Bmp)
+                {
+                    imageFormat = SKEncodedImageFormat.Bmp;
+                }
+                else if (format == ImageEncodingFormat.Dng)
+                {
+                    imageFormat = SKEncodedImageFormat.Dng;
+                }
+                else if (format == ImageEncodingFormat.Gif)
+                {
+                    imageFormat = SKEncodedImageFormat.Gif;
+                }
+                else if (format == ImageEncodingFormat.Ico)
+                {
+                    imageFormat = SKEncodedImageFormat.Ico;
+                }
+                else if (format == ImageEncodingFormat.Jpeg)
+                {
+                    imageFormat = SKEncodedImageFormat.Jpeg;
+                }
+                else if (format == ImageEncodingFormat.Ktx)
+                {
+                    imageFormat = SKEncodedImageFormat.Ktx;
+                }
+                else if (format == ImageEncodingFormat.Pkm)
+                {
+                    imageFormat = SKEncodedImageFormat.Pkm;
+                }
+                else if (format == ImageEncodingFormat.Png)
+                {
+                    imageFormat = SKEncodedImageFormat.Png;
+                }
+                else if (format == ImageEncodingFormat.Wbmp)
+                {
+                    imageFormat = SKEncodedImageFormat.Wbmp;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("format", "The image encoding format specified is not available when using SkiaSharp for processing.");
+                }
+
+                return image.Encode(imageFormat, quality).AsStream(true);
+            }
+        }
+
+        /// <summary>
+        /// Resizes the image to the specified width and height.
+        /// </summary>
+        /// <param name="width">The width in pixels to resize the image</param>
+        /// <param name="height">The height in pixels to resize the image</param>
+        public override void Resize(int width, int height)
+        {
+            var resizedBitmap = _bitmap.Resize(new SKImageInfo(width, height), SKBitmapResizeMethod.Lanczos3);
+
+            _bitmap.Dispose();
+
+            _bitmap = resizedBitmap;
+        }
+
+        /// <summary>
+        /// Crops the image using the specified aspect ratio.
+        /// </summary>
+        /// <param name="aspect">The aspect ratio to crop the image</param>
+        public override void Crop(double aspect)
+        {
+            var widthFactor = aspect;
+            var heightFactor = 1.0 / aspect;
+
+            SKRectI rect;
+
+            var desiredHeight = (int)(_bitmap.Width * heightFactor);
+
+            if (desiredHeight <= _bitmap.Height)
+            {
+                var difference = _bitmap.Height - desiredHeight;
+
+                var offset = difference / 2;
+
+                rect = new SKRectI(0, offset, _bitmap.Width, _bitmap.Height - offset);
+            }
+            else
+            {
+                var desiredWidth = (int)(_bitmap.Height * widthFactor);
+
+                var difference = _bitmap.Width - desiredWidth;
+
+                var offset = difference / 2;
+
+                rect = new SKRectI(offset, 0, _bitmap.Width - offset, _bitmap.Height);
+            }
+
+            using (var image = SKImage.FromBitmap(_bitmap))
+            {
+                using (var croppedImage = image.Subset(rect))
+                {
+                    var croppedBitmap = SKBitmap.FromImage(croppedImage);
+
+                    _bitmap.Dispose();
+
+                    _bitmap = croppedBitmap;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Removes the transparency from the image.
+        /// </summary>
+        /// <param name="color">The color to make the background of the image</param>
+        public override void RemoveTransparency(Color color)
+        {
+            var skiaColor = new SKColor(color.R, color.G, color.B);
+
+            var coloredBitmap = new SKBitmap(_bitmap.Width, _bitmap.Height);
+
+            using (var canvas = new SKCanvas(coloredBitmap))
+            {
+                canvas.DrawColor(skiaColor);
+
+                canvas.DrawBitmap(_bitmap, 0, 0);
+            }
+
+            _bitmap.Dispose();
+
+            _bitmap = coloredBitmap;
+        }
+
+        /// <summary>
+        /// Disposes the image from memory.
+        /// </summary>
+        public override void Dispose()
+        {
+            _bitmap.Dispose();
+        }
+    }
+}
